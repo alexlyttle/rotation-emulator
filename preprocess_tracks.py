f6d384e1344811b8a28af4b2bf1ae6e6fe81f50d
alexlyttle <a.j.lyttle@outlook.com>
2021-05-13 18:32:19 +0100

    Add try and except

--- preprocess_tracks.py
+++ preprocess_tracks.py
@@ -3,21 +3,24 @@ import numpy as np
 
 from track import Track
 
-DATA_DIR = './data/example_tracks'
-FILENAMES = os.listdir(DATA_DIR)
-HDFPATH = 'data/example_grid.h5'
+DATA_DIR = '/rds/projects/d/daviesgr-alex-phd/rotation_grid/rot_evolved'
+FILENAMES = [fn for fn in os.listdir(DATA_DIR) if fn.startswith('m') and fn.endswith('.csv')]
+HDFPATH = '/rds/projects/d/daviesgr-alex-phd/repos/alexlyttle/rotation-emulator/data/yrec_grid.h5'
 
 COLUMNS = ['EEP', 'M/Msun', 'Age(Gyr)', '[Fe/H]', 'fk', 'Rocrit', 
-           'Prot(days)', 'log(Teff)(K)', 'R/Rsun', 'Z/X(surface)']
+           'Prot(days)', 'log(Teff)(K)', 'R/Rsun', 'log(Z/X)(surface)']
 
 data = []
 
 # Loop through track data files and calculate EEPs and then append to data
 for i, fn in enumerate(FILENAMES):
-    track = Track(os.path.join(DATA_DIR, fn))
-    track.df['EEP'] = track.calculate_eep()
-    data.append(track.df.loc[track.eep_index['ZAMS']:track.eep_index['TAMS'], COLUMNS].to_numpy())
-
+    print(fn)
+    try:
+        track = Track(os.path.join(DATA_DIR, fn))
+        track.df['EEP'] = track.calculate_eep()
+        data.append(track.df.loc[track.eep_index['ZAMS']:track.eep_index['TAMS'], COLUMNS].to_numpy())
+    except Exception as err:
+        print(err)
 data = np.concatenate(data)
 
 h5f = h5py.File(HDFPATH, 'w')
